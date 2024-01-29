6c99547cad364d7f02d538ba1cf65b1464340f24
Darshan <darshanvkalola@gmail.com>
2024-01-29 17:08:27 -0500

    refactor: Sync Center field names on backend and frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,17 +5,17 @@ import { CenterCardsGrid } from './home/center_cards_grid'
 const MOCK_CENTERS_DATA = [
   {
     id: '1234',
-    title: 'Bay Cove Substance Abuse Center',
+    name: 'Bay Cove Substance Abuse Center',
     location: '66 Canal St, Boston, MA 02114',
     centerType: 'Clinical Stabilization Services',
-    imageUrl: 'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80'
+    image: 'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80'
   },
   {
     id: '1234',
-    title: 'Bay Cove Substance Abuse Center',
+    name: 'Bay Cove Substance Abuse Center',
     location: '66 Canal St, Boston, MA 02114',
     centerType: 'Clinical Stabilization Services',
-    imageUrl: 'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80'
+    image: 'https://images.unsplash.com/photo-1437719417032-8595fd9e9dc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=600&q=80'
   }
 ]
 
