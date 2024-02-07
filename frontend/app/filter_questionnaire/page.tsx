e0bc1e212e21ea248ebc57f084817dd46f6b7f38
Darshan <darshanvkalola@gmail.com>
2024-02-07 17:39:29 -0500

    feat: Move gender input into filter questionnaire main page

--- frontend/app/filter_questionnaire/page.tsx
+++ frontend/app/filter_questionnaire/page.tsx
@@ -1,4 +1,8 @@
-import { Container, Stack } from '@mantine/core'
+'use client'
+
+import { useState } from 'react'
+
+import { Container, Stack, Fieldset, Radio, Group } from '@mantine/core'
 
 import MainNavbar from '@/app/navbar/main_navbar'
 
@@ -11,9 +15,32 @@ export default function FilterQuestionnairePage (): any {
       <main>
         <Container pt="lg">
           <Stack gap="md">
+            <Fieldset legend="Demographic Information">
+              <SexInput />
+            </Fieldset>
           </Stack>
         </Container>
       </main>
     </>
+  )
+}
+
+function SexInput (): any {
+  const [value, setValue] = useState('react')
+
+  return (
+    <Radio.Group
+      value={value}
+      onChange={setValue}
+      name="sexInput"
+      label="What is your sex?"
+      withAsterisk
+    >
+      <Group mt="xs">
+        <Radio value="male" label="Male" />
+        <Radio value="female" label="Female" />
+        <Radio value="other" label="Other" />
+      </Group>
+    </Radio.Group>
   )
 }
