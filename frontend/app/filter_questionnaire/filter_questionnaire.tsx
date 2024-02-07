1643040dfda58af537d3cba7421685f1d2bf8687
Darshan <darshanvkalola@gmail.com>
2024-02-07 12:07:16 -0500

    feat: Add filter button and add modal popup for the filter questionnaire

---
+++ frontend/app/filter_questionnaire/filter_questionnaire.tsx
@@ -0,0 +1,12 @@
+import { Stack, Text, Title } from '@mantine/core'
+
+export default function FilterQuestionnaire (): any {
+  return (
+    <Stack gap="md">
+      <Title order={3}>Filters</Title>
+      <Text>
+
+      </Text>
+    </Stack>
+  )
+}
--- frontend/app/navbar/second_navbar.tsx
+++ frontend/app/navbar/second_navbar.tsx
@@ -1,9 +1,15 @@
 'use client'
 
-import { Group, Flex, Button, ActionIcon, Tooltip } from '@mantine/core'
-import { IconArrowGuide, IconHome, IconPillOff, IconReportMedical } from '@tabler/icons-react'
+import { useDisclosure } from '@mantine/hooks'
+
+import { Group, Flex, Button, ActionIcon, Tooltip, Modal } from '@mantine/core'
+import { IconAdjustments, IconArrowGuide, IconHome, IconPillOff, IconReportMedical } from '@tabler/icons-react'
+
+import FilterQuestionnaire from '../filter_questionnaire/filter_questionnaire'
 
 export default function SecondNavbar (): any {
+  const [opened, { open, close }] = useDisclosure(false)
+
   return (
     <Flex justify="space-between" px="md" pt="md">
       <Group gap="xl">
@@ -29,8 +35,11 @@ export default function SecondNavbar (): any {
         </Tooltip>
       </Group>
       <Group>
-        <Button>
-          Find for me
+        <Modal opened={opened} onClose={close} title="Filters" centered>
+          <FilterQuestionnaire />
+        </Modal>
+        <Button leftSection={<IconAdjustments size={14} />} variant="outline" color="black" size="md" onClick={open}>
+          Filters
         </Button>
       </Group>
     </Flex>
