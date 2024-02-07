6df393cdb71dca80f820f6d1472c0def1af74975
Darshan <darshanvkalola@gmail.com>
2024-02-07 16:47:14 -0500

    feat: Add gender input to filter questionnaire

--- frontend/app/filter_questionnaire/filter_questionnaire.tsx
+++ frontend/app/filter_questionnaire/filter_questionnaire.tsx
@@ -1,12 +1,31 @@
-import { Stack, Text, Title } from '@mantine/core'
+import { useState } from 'react'
+
+import { Fieldset, Stack, Radio } from '@mantine/core'
 
 export default function FilterQuestionnaire (): any {
   return (
     <Stack gap="md">
-      <Title order={3}>Filters</Title>
-      <Text>
-
-      </Text>
+      <Fieldset legend="Demographic Information">
+        <GenderInput />
+      </Fieldset>
     </Stack>
+  )
+}
+
+function GenderInput (): any {
+  const [value, setValue] = useState('react')
+
+  return (
+    <Radio.Group
+      value={value}
+      onChange={setValue}
+      name="genderInput"
+      label="What is your gender?"
+      withAsterisk
+    >
+      <Radio value="male" label="Male" />
+      <Radio value="female" label="Female" />
+      <Radio value="other" label="Other" />
+    </Radio.Group>
   )
 }
