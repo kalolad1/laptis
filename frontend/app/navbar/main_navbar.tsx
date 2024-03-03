cac5315c0d4f9555e811613d108d613eef4d2d60
Darshan <darshanvkalola@gmail.com>
2024-03-03 13:40:33 -0500

    feat: Add logo to navbar, favicon

--- frontend/app/navbar/main_navbar.tsx
+++ frontend/app/navbar/main_navbar.tsx
@@ -4,7 +4,8 @@ import {
   Image,
   Group,
   Button,
-  Flex
+  Flex,
+  Title
 } from '@mantine/core'
 import { IconAdjustments } from '@tabler/icons-react'
 import classes from './main_navbar.module.css'
@@ -13,13 +14,16 @@ import Link from 'next/link'
 export default function MainNavbar (): any {
   return (
     <Flex className={classes.flex_container} justify="space-between" p="md">
-      <Group>
-        <Image
-          src="/logo.png"
-          width={40}
-          height={40}
-        />
-      </Group>
+      <Link href="/" style={{ textDecoration: 'none', color: 'inherit' }} passHref>
+        <Group>
+          <Image
+            src="/logo.svg"
+            width={50}
+            height={50}
+          />
+          <Title>Laptis</Title>
+        </Group>
+      </Link>
       <Group>
         <Link href="/filter_questionnaire" passHref>
           <Button leftSection={<IconAdjustments size={14} />} variant="outline" color="black" size="md">
