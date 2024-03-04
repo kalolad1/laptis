d049a8e1200681ae076e0cbf287bb6e73be075b8
Darshan <darshanvkalola@gmail.com>
2024-03-03 20:08:25 -0500

    Revert "fix: Delete extra slash in endpoint"
    This reverts commit a59d504c602b1ce76d81619814f09cb6f85eb269.

--- frontend/app/constants/endpoints.ts
+++ frontend/app/constants/endpoints.ts
@@ -1,5 +1,5 @@
 // API
 export const BASE_API_ENDPOINT = process.env.NEXT_PUBLIC_BASE_API_ENDPOINT
 
-export const GET_CENTERS_ENDPOINT = BASE_API_ENDPOINT + 'centers'
-export const FILTER_CENTERS_ENDPOINT = BASE_API_ENDPOINT + 'filter_centers'
+export const GET_CENTERS_ENDPOINT = BASE_API_ENDPOINT + '/centers'
+export const FILTER_CENTERS_ENDPOINT = BASE_API_ENDPOINT + '/filter_centers'
