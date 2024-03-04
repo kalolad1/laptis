c9a78119b6565ec67c76927ba9fc1122e474f935
Darshan <darshanvkalola@gmail.com>
2024-03-03 19:37:21 -0500

    build: Add frontend CORS origin

--- backend/laptis/settings.py
+++ backend/laptis/settings.py
@@ -31,6 +31,7 @@ DEBUG = "True"
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "https://localhost:3000",
+    "https://laptis-nknr2tak7-kalolad1.vercel.app"
 ]
 
 AUTH_USER_MODEL = "core.User"
