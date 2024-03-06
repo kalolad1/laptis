2c697b74b3cd1f70d4e93a0e95b7a81e6d1bec54
Darshan <darshanvkalola@gmail.com>
2024-03-05 23:32:31 -0500

    fix: Disable React devTools console message

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -16,5 +16,17 @@ export default withBundleAnalyzer({
   typescript: {
     ignoreBuildErrors: true,
   },
+    webpack: (
+      config,
+    { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+  ) => {
+    config.plugins = [...config.plugins,
+      new webpack.DefinePlugin({
+        '__REACT_DEVTOOLS_GLOBAL_HOOK__': '({ isDisabled: true })'
+      })
+    ]
+    // Important: return the modified config
+    return config
+  }
 });
 
