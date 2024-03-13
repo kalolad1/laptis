4c1cdf1c9a8faa2b68f8435224a10f76b1864864
Darshan <darshanvkalola@gmail.com>
2024-03-12 23:49:28 -0400

    feat: Add patient tab content

--- frontend/app/provider_home/MainContent.tsx
+++ frontend/app/provider_home/MainContent.tsx
@@ -7,7 +7,7 @@ import PatientTabContent from '@/app/provider_home/PatientTabContent'
 
 export default function MainContent (): JSX.Element {
   return (
-    <Stack>
+    <Stack justify='flex-start' gap='lg' style={{ height: '100vh' }}>
       <Navbar></Navbar>
       <PatientTabContent></PatientTabContent>
     </Stack>
--- frontend/app/provider_home/PatientTabContent.tsx
+++ frontend/app/provider_home/PatientTabContent.tsx
@@ -1,24 +1,52 @@
+import { Button, Flex, Table } from '@mantine/core'
+import { IconSearch, IconUserCircle } from '@tabler/icons-react'
+
 export default function PatientTabContent (): JSX.Element {
   return (
-    <div>
+    <Flex p='lg'>
       <PatientTable />
       <NewPatientButton />
-    </div>
+    </Flex>
   )
 }
 
 function PatientTable (): JSX.Element {
+  const patients = [
+    { name: 'Darshan Kalola', applicationStatus: 'Applied' },
+    { name: 'Sudeep Peddireddy', applicationStatus: 'Denied' },
+    { name: 'Andrew Steen', applicationStatus: 'Accepted' }
+  ]
+
+  const rows = patients.map((patient) => (
+    <Table.Tr key={patient.name}>
+      <Table.Td>
+        <IconUserCircle />
+      </Table.Td>
+      <Table.Td>{patient.name}</Table.Td>
+      <Table.Td>{patient.applicationStatus}</Table.Td>
+      <Table.Td>
+        <a
+          onClick={(event) => {
+            event.preventDefault()
+          }}
+        >
+          <IconSearch />
+        </a>
+      </Table.Td>
+    </Table.Tr>
+  ))
+
   return (
-    <div>
-      PatientTable
-    </div>
+    <Table highlightOnHover>
+      <Table.Tbody>{rows}</Table.Tbody>
+    </Table>
   )
 }
 
 function NewPatientButton (): JSX.Element {
   return (
-    <div>
-      NewPatientButton
-    </div>
+    <Button style={{ position: 'absolute', bottom: 24, right: 24 }}>
+      New Patient
+    </Button>
   )
 }
