0408c3a10a9b7fb2460f84889e55cac2b9363620
Darshan <darshanvkalola@gmail.com>
2024-03-14 14:20:54 -0400

    feat: Add bool flag to user to indicate patient and add patient profile

--- backend/core/models/user.py
+++ backend/core/models/user.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    is_patient = models.BooleanField(default=False)
+
+
+class Patient(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    first_name = models.CharField(max_length=200, default="")
+    last_name = models.CharField(max_length=200, default="")
+    age = models.IntegerField(default=-1)
+
