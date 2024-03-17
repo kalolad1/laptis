c069f306d7bfd00f6cb87187d2bd74184b2ecc33
Darshan <darshanvkalola@gmail.com>
2024-03-17 13:29:26 -0400

    refactor: Remove unused env code from settings

--- backend/laptis/settings.py
+++ backend/laptis/settings.py
@@ -6,8 +6,6 @@ import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
