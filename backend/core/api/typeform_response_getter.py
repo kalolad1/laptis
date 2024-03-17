2083358042a8730f099bb676bf1403e296cdd5ce
Darshan <darshanvkalola@gmail.com>
2024-03-17 11:37:32 -0400

    refactor: Add private python fields

--- backend/core/api/typeform_response_getter.py
+++ backend/core/api/typeform_response_getter.py
@@ -5,20 +5,20 @@ from typing import Any, Dict
 
 class TypeformResponseGetter:
     def __init__(self, form_id: str, response_id: str) -> None:
-        self.form_id = form_id
-        self.response_id = response_id
+        self._form_id = form_id
+        self._response_id = response_id
 
     def get_answers(self) -> Dict[Any, Any]:
         TYPEFORM_API_URL = "https://api.typeform.com"
-        BEARER_TOKEN = os.environ.get("TYPEFORM_BEARER_TOKEN")
+        TYPEFORM_BEARER_TOKEN = os.environ.get("TYPEFORM_BEARER_TOKEN")
 
         request_url = (
-            f"{TYPEFORM_API_URL}/forms/{self.form_id}"
-            f"/responses?included_response_ids={self.response_id}"
+            f"{TYPEFORM_API_URL}/forms/{self._form_id}"
+            f"/responses?included_response_ids={self._response_id}"
         )
         response = requests.get(
             request_url,
-            headers={"Authorization": f"Bearer {BEARER_TOKEN}"},
+            headers={"Authorization": f"Bearer {TYPEFORM_BEARER_TOKEN}"},
         )
 
         answers = {}
--- backend/core/center_filterer.py
+++ backend/core/center_filterer.py
@@ -9,38 +9,38 @@ class CenterFilterer:
     def __init__(
         self, user: User, patient_application_context: PatientApplicationContext
     ) -> None:
-        self.user = user
-        self.patient_application_context = patient_application_context
+        self._user = user
+        self._patient_application_context = patient_application_context
 
     def get_centers(self) -> QuerySet[Center]:
         # Is the patient's sex eligible for the center?
         centers = Center.objects.filter(
-            eligible_sexes__contains=[self.user.patient.sex]
+            eligible_sexes__contains=[self._user.patient.sex]
         )
 
         # Is the patient's age eligible for the center?
-        centers = centers.filter(eligible_age_minimum__lte=self.user.patient.age)
-        centers = centers.filter(eligible_age_maximum__gte=self.user.patient.age)
+        centers = centers.filter(eligible_age_minimum__lte=self._user.patient.age)
+        centers = centers.filter(eligible_age_maximum__gte=self._user.patient.age)
 
         # Are the patient's medications eligible with the center?
-        mat = self.user.patient.using_medication_assisted_therapies
+        mat = self._user.patient.using_medication_assisted_therapies
         centers = centers.filter(eligible_medications__contains=mat)
 
         # Are the patient's mental diagnoses eligible with the center?
-        mhd = self.user.patient.mental_health_diagnoses
+        mhd = self._user.patient.mental_health_diagnoses
         centers = centers.filter(eligible_mental_health_diagnoses__contains=mhd)
 
         # Is the patient's health insurance accepted by the center?
         centers = centers.filter(
-            eligible_health_insurances__contains=[self.user.patient.health_insurance]
+            eligible_health_insurances__contains=[self._user.patient.health_insurance]
         )
 
         # Is the patient's mobility compatible with the center?
-        if self.user.patient.has_disability:
+        if self._user.patient.has_disability:
             centers = centers.exclude(is_disability_compatible=False)
 
         # Is the patient open to faith-based treatment?
-        if not self.user.patient.is_open_to_faith_based_treatment:
+        if not self._user.patient.is_open_to_faith_based_treatment:
             centers = centers.exclude(is_faith_based_treatment=True)
 
         return centers
