e0ef864251b08e35601d79997724653988656edc
Darshan <darshanvkalola@gmail.com>
2024-03-25 17:58:26 -0400

    feat: Redirect to dashboard if logged in

--- frontend/app/home/layout.tsx
+++ frontend/app/home/layout.tsx
@@ -1,6 +1,22 @@
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+import { ACCESS_TOKEN } from '@/app/constants/local_storage'
+
 import MainNavbar from '@/app/shared_components/navbar/home/HomeNavbar'
 
 export default function HomeLayout ({ children }: { children: any }): JSX.Element {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (localStorage.getItem(ACCESS_TOKEN) !== null) {
+      // TODO: Check if provider or center
+      router.push('/provider_dashboard/patients')
+    }
+  }, [router])
+
   return (
     <>
       <header>
